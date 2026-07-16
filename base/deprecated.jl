881ce6d9145519c1ea73b8077edce391ef49afdf
Kristoffer Carlsson <kcarlsson89@gmail.com>
2026-07-16 12:37:28 +0200

    loading: structured environment parsing and move environment code into its own file

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -578,6 +578,29 @@ to_power_type(x) = oftype(x*x, x)
 # BEGIN 1.14 deprecations
 
 # Revise calls this
+function explicit_manifest_entry_load_spec(manifest_file::String, pkg::PkgId, entry::Dict{String,Any})::Union{Nothing, Missing, PkgLoadSpec}
+    syntax_version = syntax_table_version(get(entry, "syntax", nothing))
+    if syntax_version === nothing || syntax_version <= NON_VERSIONED_SYNTAX
+        syntax_version = NON_VERSIONED_SYNTAX
+    end
+
+    entryfile = get(entry, "entryfile", nothing)::Union{Nothing, String}
+    path = get(entry, "path", nothing)::Union{Nothing, String}
+    if path !== nothing
+        root = normpath(abspath(dirname(manifest_file), path))
+        return PkgLoadSpec(entry_path(root, pkg.name, entryfile), syntax_version)
+    end
+
+    hash = get(entry, "git-tree-sha1", nothing)::Union{Nothing, String}
+    if hash === nothing
+        spec = _locate_package(stdlib_env(), pkg)
+        return spec isa PkgLoadSpec && isfile(spec.path) ? spec : nothing
+    end
+    root = find_depot_package_root(pkg, SHA1(hash))
+    root === nothing && return missing
+    return PkgLoadSpec(entry_path(root, pkg.name, entryfile), syntax_version)
+end
+
 function explicit_manifest_entry_path(args...)
     spec = explicit_manifest_entry_load_spec(args...)
     spec === nothing && return nothing
